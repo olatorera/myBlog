9600d52fd11d3b7566cd1c088e2be0a794072726
olatorera <sanusiolatorera@gmail.com>
2022-06-27 12:27:07 +0100

    useState

---
+++ src/Components/BlogList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const BlogList = ({ blogs, title, handleDelete }) => {
+// const blogs = props.blogs;
+
+
+  return (
+    <div className="blog-list">
+        <h2>{title}</h2>
+       {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>written by {blog.author}</p>
+          <h4>{blog.body}</h4>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default BlogList
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blog, setBlog] = useState([
+   const [blogs, setBlogs] = useState([
     { title: "My new Page", body: "lorem ipsum ...", author: "TeeDev", id: 1 },
     { title: "My new Page", body: "lorem ipsum ...", author: "Paul", id: 2 },
     { title: "My new Page", body: "lorem ipsum ...", author: "Abayomi", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="homepage">
-      {blog.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>written by {blog.author}</p>
-          <h4>{blog.body}</h4>
-        </div>
-      ))}
+
+      < BlogList blogs={blogs} title='All Blogs!!!'  handleDelete={handleDelete} /> 
     </div>
   );
 };
